a339946d0f87b6f0f4b29dcd40bfa91a86694ec9
sanjay <sanu2336@gmail.com>
2025-02-16 15:58:13 +0530

    downgraded react to 18 for three.js and removed usage of timeline

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React from "react";
-import Timeline from "./Timeline";
-
+import LightStreak from "./LightStreak";
 const Hero: React.FC = () => {
   return (
     <section className="flex flex-col justify-center items-center min-h-screen text-center px-4 relative bg-gradient-to-t from-[rgba(0,80,15,0.15)] to-transparent before:absolute before:inset-x-0 before:bottom-0 before:h-3/4 before:bg-gradient-to-t before:from-[rgba(0,80,15,0.4)] before:to-transparent overflow-clip">
@@ -14,10 +13,7 @@ const Hero: React.FC = () => {
         <p className="text-xl md:text-[36px]">A Journal You Can Query Into</p>
       </div>
 
-      {/* Timeline */}
-      <div className="absolute bottom-20 left-0 right-0 mt-8">
-        <Timeline />
-      </div>
+      <LightStreak />
     </section>
   );
 };
