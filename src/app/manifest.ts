64b540a94fe8cdc79aeb7fb13331164527d7e64c
sanjay <sanu2336@gmail.com>
2025-03-01 01:20:52 +0530

    Update icons and social images to use actual Zeniary logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,19 +12,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
+        src: "/zeniary-logo.svg",
         sizes: "any",
-        type: "image/x-icon",
+        type: "image/svg+xml",
       },
       {
-        src: "/icon-192.png",
+        src: "/icon-192.svg",
         sizes: "192x192",
-        type: "image/png",
+        type: "image/svg+xml",
       },
       {
-        src: "/icon-512.png",
+        src: "/icon-512.svg",
         sizes: "512x512",
-        type: "image/png",
+        type: "image/svg+xml",
       },
     ],
   };
